3c779a9d3f836569243ac292d26654f85ada946b
Marcelo Lupatini <mar.lupatini@gmail.com>
2022-08-08 22:47:02 -0300

    Cap. 1, M1 #3 - S3, Finalização do Im Here

--- 01-imhere/src/screens/Home/index.tsx
+++ 01-imhere/src/screens/Home/index.tsx
@@ -1,51 +1,43 @@
+import { useState } from 'react';
 import { Alert, FlatList, Text, TextInput, TouchableOpacity, View } from 'react-native';
+
 import { Participant } from '../../components/Participant';
 
 import { styles } from './styles';
 
 export function Home() {
-  const participants = [
-    'Marcelo',
-    'Luis',
-    'Jizar',
-    'Larissa',
-    'Mateus',
-    'Rebeca',
-    'Giovanna',
-    'Fabricio',
-    'Arthur',
-    'Basso',
-    'Bohrer',
-    'Torben',
-    'Vascon',
-    'Yasmin',
-    'Kemilly',
-    'Felipe',
-  ];
+  const [participants, setParticipants] = useState<string[]>([]);
+  const [participantName, setParticipantName] = useState('');
 
-  function handlePartipantAdd() {
-    if (participants.includes('Marcelo')) {
+  function handlePartipantAdd(name: string) {
+    if (participants.includes(name)) {
       return Alert.alert(
         'Participante existe',
-        'Já existe um participante na lista com este nome.',
-        [
-          {
-            text: 'Sim',
-            onPress: () => Alert.alert('Deletado')
-          },
-          {
-            text: 'Não',
-            style: 'cancel',
-          },
-        ]
+        'Já existe um participante na lista com este nome.'
       );
     }
+
+    setParticipants(previousState => [...previousState, name]);
+    setParticipantName('');
   }
 
   function handlePartipantRemove(name: string) {
     Alert.alert(
       'Remover participante',
-      `Remover o participante ${name}?`
+      `Remover o participante ${name}?`,
+      [
+        {
+          text: 'Sim',
+          onPress: () => setParticipants(
+            (previousState) =>
+              previousState.filter(participant => participant !== name)
+          ),
+        },
+        {
+          text: 'Não',
+          style: 'cancel',
+        },
+      ]
     );
   }
 
@@ -63,11 +55,12 @@ export function Home() {
           style={styles.input}
           placeholder="Nome do participante"
           placeholderTextColor="#6B6B6B"
-          keyboardType="numeric"
+          onChangeText={setParticipantName}
+          value={participantName}
         />
         <TouchableOpacity
           style={styles.button}
-          onPress={handlePartipantAdd}
+          onPress={() => handlePartipantAdd(participantName)}
         >
           <Text style={styles.buttonText}>
             +
